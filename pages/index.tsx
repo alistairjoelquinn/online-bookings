54474af547127b434100b94ec0ba74a67e7b7474
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-03-31 14:22:40 +0200

    refac improves prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,20 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
-  const name = 'alistair';
-  const me = 'hell';
-  return (
-    <>
-      <Head>
-        <title>Home Page</title>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <div className="flex min-h-screen flex-col items-center justify-center py-2">
-        <h1 className="text-4xl text-red-500">I am the HOME PAGE</h1>
-      </div>
-    </>
-  );
+    console.log('hello: ');
+    const name = 'alistair';
+    const me = 'hell';
+    return (
+        <>
+            <Head>
+                <title>Home Page</title>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+            <div className="flex min-h-screen flex-col items-center justify-center py-2">
+                <h1 className="text-4xl text-red-500">I am the HOME PAGE</h1>
+            </div>
+        </>
+    );
 };
 
 export default Home;
