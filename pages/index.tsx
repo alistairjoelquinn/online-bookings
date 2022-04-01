d68e464af23eda0d79dfe1416a09209229f9e7b7
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-04-01 10:56:41 +0200

    adds grommet icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
                 <h3 className="py-6 text-5xl italic text-myblack">alistair quinn</h3>
                 <hr className="h-0.5 w-4/6 rounded border-none bg-myblack" />
             </header>
-            <div className="absolute -z-10 h-screen min-w-full max-w-3xl -translate-x-96 -translate-y-96 -rotate-45 bg-myyellow" />
+            <div className="absolute top-0 -left-40 -z-10 h-screen min-w-full max-w-3xl -translate-x-96 -translate-y-96 -rotate-45 bg-myyellow" />
             <main className="flex w-full flex-col items-center font-dank text-myblack">
                 <section className="w-4/6 text-xl">
                     <p className="pt-6 pb-6">Hi, I&#39;m Alistair.</p>
