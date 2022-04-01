304fd4efa8d7427f42b7fa05b7669aa4d80d08cf
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-04-01 09:33:18 +0200

    adds welcome paragraph

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,23 @@ const Home: NextPage = () => {
     return (
         <>
             <Head>
-                <title>alistarquinn.com</title>
+                <title>Full Stack Web Solutions</title>
                 <link rel="icon" href="/favicon.png" />
             </Head>
-            <div className="flex min-h-screen flex-col items-center justify-start py-2">
-                <h1 className="pt-6 pb-4 font-lora text-4xl italic text-red-500">alistair quinn</h1>
-                <div className="h-0.5 w-4/6 rounded bg-red-500" />
-            </div>
+            <header className="flex flex-col items-center justify-start py-2">
+                <h3 className="pt-6 pb-4 font-lora text-4xl italic text-red-500">alistair quinn</h3>
+                <hr className="h-0.5 w-4/6 rounded border-none bg-red-500" />
+            </header>
+            <main className="flex w-full flex-col items-center font-josefin">
+                <section className="w-4/6">
+                    <p>Hi, I&#39;m Alistair.</p>
+                    <p>
+                        I&#39;m a developer from Glasgow, Scotland, who is passionate about education and empowering
+                        people through tech. Currently, I&#39;m working as a Full Stack Web Development teacher at an
+                        exciting school in Berlin, Germany, called Spiced Academy.
+                    </p>
+                </section>
+            </main>
         </>
     );
 };
