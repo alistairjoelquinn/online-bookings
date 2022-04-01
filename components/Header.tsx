ef0cf6ca039961987545a6f9019eb5547093c893
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-04-01 11:44:21 +0200

    moves header to own file

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => (
+    <header className="flex flex-col items-center justify-start py-4 font-dank">
+        <h3 className="py-6 text-5xl italic text-myblack">alistair quinn</h3>
+        <hr className="h-0.5 w-4/6 rounded border-none bg-myblack" />
+    </header>
+);
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+
 import MainSection from '../components/MainSection';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 const Home: NextPage = () => (
     <>
@@ -9,10 +11,7 @@ const Home: NextPage = () => (
             <title>Alistair Quinn - Web Developer</title>
             <link rel="icon" href="/favicon.png" />
         </Head>
-        <header className="flex flex-col items-center justify-start py-4 font-dank">
-            <h3 className="py-6 text-5xl italic text-myblack">alistair quinn</h3>
-            <hr className="h-0.5 w-4/6 rounded border-none bg-myblack" />
-        </header>
+        <Header />
         <div className="absolute top-0 -left-40 -z-10 h-screen min-w-full max-w-3xl -translate-x-96 -translate-y-96 -rotate-45 bg-myyellow" />
         <MainSection />
         <Footer />
