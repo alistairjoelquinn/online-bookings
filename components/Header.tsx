981c4ade056996548518d4666b0bda241f2876c5
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-04-04 09:38:31 +0200

    header slides off to the right

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,14 @@
+import { Parallax } from 'react-scroll-parallax';
+
 const Header = () => (
     <header className="flex flex-col items-center justify-start py-4 font-dank">
-        <h3 className="py-6 text-5xl italic text-myblack">alistair quinn</h3>
+        <Parallax
+            translateY={['-1230px', '200px']}
+            translateX={[-669, 100]}
+            className="py-6 text-5xl italic text-myblack"
+        >
+            <h3>alistair quinn</h3>
+        </Parallax>
         <hr className="h-0.5 w-4/6 rounded border-none bg-myblack" />
     </header>
 );
