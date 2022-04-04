7531d40558cdc66bba0fd36118b0654fbed60686
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-04-04 09:57:44 +0200

    header just blurs on exit

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,7 @@ import { Parallax } from 'react-scroll-parallax';
 
 const Header = () => (
     <header className="flex flex-col items-center justify-start py-4 font-dank">
-        <Parallax
-            translateY={['-1230px', '200px']}
-            translateX={[-669, 100]}
-            className="py-6 text-5xl italic text-myblack"
-        >
+        <Parallax opacity={[10, 0]} speed={0} className="py-6 text-5xl italic text-myblack">
             <h3>alistair quinn</h3>
         </Parallax>
         <hr className="h-0.5 w-4/6 rounded border-none bg-myblack" />
