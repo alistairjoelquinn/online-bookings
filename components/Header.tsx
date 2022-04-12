73de52bb1ee0d7c417e470aeb54f6040af7edd7d
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-04-12 16:39:10 +0200

    updates husky

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
 const Header = () => (
     <header className="flex flex-col items-center justify-start py-4 font-dank">
-        <h3>Felicity Quinn</h3>
-        <hr className="h-0.5 w-4/6 rounded border-none bg-myblack" />
+        <h3 className="border-transparent hover:border-purple-500 p-6 text-4xl transition duration-150">
+            Felicity Quinn
+        </h3>
+        <hr className="h-0.5 w-4/6 rounded border-none bg-myblack " />
     </header>
 );
 
