8c02b1cc3c5900ad07b016046abbdee4bb068be1
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-04-12 14:20:22 +0200

    removes parallax

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,6 @@
-import { Parallax } from 'react-scroll-parallax';
-
 const Header = () => (
     <header className="flex flex-col items-center justify-start py-4 font-dank">
-        <Parallax opacity={[10, 0]} speed={0} className="py-6 text-5xl italic text-myblack">
-            <h3>alistair quinn</h3>
-        </Parallax>
+        <h3>Felicity Quinn</h3>
         <hr className="h-0.5 w-4/6 rounded border-none bg-myblack" />
     </header>
 );
--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -1,22 +1,8 @@
-import { Parallax } from 'react-scroll-parallax';
-
 const MainSection = () => {
-    console.log('main section: ');
+    console.log('main section');
     return (
         <main style={{ height: '1000px' }} className="flex w-full flex-col items-center font-dank text-myblack">
-            <Parallax speed={2} opacity={[5, 0]} className="w-4/6 text-xl">
-                <section>
-                    <p className="pt-6 pb-6">Hi, I&#39;m Alistair.</p>
-                    <p className="pb-6">
-                        I&#39;m a developer from Glasgow, Scotland, who is passionate about education and empowering
-                        people through tech.
-                    </p>
-                    <p className="pb-6">
-                        Currently, I&#39;m working as a Full Stack Web Development teacher at an exciting school in
-                        Berlin, Germany, called Spiced Academy.
-                    </p>
-                </section>
-            </Parallax>
+            This is the main body
         </main>
     );
 };
