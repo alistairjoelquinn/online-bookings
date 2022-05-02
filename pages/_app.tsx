d12e1db69e30cbe4d165dab99e2ca3a48f38a509
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-02 15:00:35 +0200

    moves doc head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 const App = ({ Component, pageProps }: AppProps) => (
-    <ParallaxProvider>
-        <Component {...pageProps} />
-    </ParallaxProvider>
+    <>
+        <Head>
+            <title>Felicity Quinn - English Teacher</title>
+            <link rel="icon" href="/favicon.png" />
+        </Head>
+        <ParallaxProvider>
+            <Component {...pageProps} />
+        </ParallaxProvider>
+    </>
 );
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
 import MainSection from '../components/MainSection';
 import Footer from '../components/Footer';
@@ -7,10 +6,6 @@ import Header from '../components/Header';
 
 const Home: NextPage = () => (
     <>
-        <Head>
-            <title>Felicity Quinn - English Teacher</title>
-            <link rel="icon" href="/favicon.png" />
-        </Head>
         <Header />
         <MainSection />
         <div>test</div>
