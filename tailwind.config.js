1009390a5b91002ec4579938661d0e09c023343c
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-02 17:28:14 +0200

    refac global css though sitll no classes loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable global-require */
 /* eslint-disable prettier/prettier */
 module.exports = {
-    mode: 'jit',
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: 'media',
     theme: {
@@ -9,54 +8,12 @@ module.exports = {
             fontFamily: {
                 dank: ['DankMono', 'sans-serif'],
             },
-            typography: theme => ({
-                dark: {
-                    css: {
-                        color: theme('colors.gray.400'),
-                        '[class~="lead"]': {
-                            color: theme('colors.gray.300'),
-                        },
-                        a: {
-                            color: theme('colors.blue.400'),
-                        },
-                        strong: {
-                            color: theme('colors.white'),
-                        },
-                        'ul > li::before': {
-                            color: theme('colors.gray.600'),
-                        },
-                        h2: {
-                            color: theme('colors.white'),
-                        },
-                        figcaption: {
-                            color: theme('colors.gray.400'),
-                        },
-                        thead: {
-                            color: theme('colors.white'),
-                            borderBottomColor: theme('colors.gray.400'),
-                        },
-                        'tbody tr': {
-                            borderBottomColor: theme('colors.gray.600'),
-                        },
-                    },
-                },
-                DEFAULT: {
-                    css: {
-                        // color: theme('colors.red.600'),
-                    },
-                },
-            }),
         },
         colors: {
             mybrown: '#CF8E80',
             mypink: '#FCDDF2',
             myyellow: '#F9EA9A',
             myblack: '#2F2F2F',
-        },
-    },
-    variants: {
-        extend: {
-            typography: ['dark'],
         },
     },
     plugins: [
