65473cb416f41e2163ca24a2424cb6adfe9e523e
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-02 16:08:54 +0200

    adds home page for testing styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
+/* eslint-disable global-require */
 /* eslint-disable prettier/prettier */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        lora: ['Lora', 'serif'],
-        josefin: ['Josefin', 'sans-serif'],
-        dank: ['DankMono', 'sans-serif'],
-      },
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                lora: ['Lora', 'serif'],
+                josefin: ['Josefin', 'sans-serif'],
+                dank: ['DankMono', 'sans-serif'],
+            },
+        },
+        colors: {
+            mybrown: '#CF8E80',
+            mypink: '#FCDDF2',
+            myyellow: '#F9EA9A',
+            myblack: '#2F2F2F',
+        },
     },
-    colors: {
-      'mybrown': '#CF8E80',
-      'mypink': '#FCDDF2',
-      'myyellow': '#F9EA9A',
-      'myblack': '#2F2F2F'
-    }
-  },
-  plugins: [],
+    plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
