3ac90ef508b85a88fedb2b46a06e6d929f19e739
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-03 15:36:27 +0200

    positions main home page image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,13 @@ import Head from 'next/head';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
-    <>
+    <section className="min-h-screen">
         <Head>
             <title>Felicity Quinn - English Teacher</title>
             <link rel="icon" href="/favicon.png" />
         </Head>
         <Component {...pageProps} />
-    </>
+    </section>
 );
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from 'next';
-
-import MainSection from '../components/MainSection';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
 
 const Home: NextPage = () => (
     <>
-        <Header />
-        <MainSection />
-        <div className="text-4xl text-red-400">test</div>
-        <section className="w-screen bg-gray-600 text-center text-4xl text-green-500">
-            <p>This is so crazy</p>
-            <h1>This is VERY INTERESTNIG</h1>
-        </section>
-        <Footer />
+        <div className="m-16 h-1/4 w-1/3 overflow-hidden rounded-2xl shadow-xl">
+            <img src="img/main-image.jpg" className="h-full w-full rounded-2xl object-cover" alt="Felicity Quinn" />
+        </div>
+        <h3>Felicity Quinn</h3>
     </>
 );
 
