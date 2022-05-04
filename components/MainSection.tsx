fbf5d22a5fa4478ce23c78a74981cb93efed5b33
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-04 09:37:52 +0200

    removes unused header

--- components/MainSection.tsx
+++
@@ -1,8 +0,0 @@
-const MainSection = () => {
-    console.log('main section');
-    return (
-        <main className="flex max-h-0 w-full flex-col items-center font-dank text-myblack">This is the main body</main>
-    );
-};
-
-export default MainSection;
---
+++ pages/about.tsx
@@ -0,0 +1,8 @@
+const About = () => (
+    <>
+        <h3>Hello</h3>
+        <p>About page</p>
+    </>
+);
+
+export default About;
