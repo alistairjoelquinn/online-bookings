27e2515297592c395276e68972992022a360a5d7
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-10 10:47:39 +0200

    memoizes week values when user selected new date

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import getWeek from '../lib/getWeek';
 
 const Calendar = () => {
     const [selectedDate, setSelectedDate] = useState('');
-    console.log('selectedDate: ', selectedDate);
+    const week = useMemo<FormattedDate[] | null>(() => getWeek(selectedDate), [selectedDate]);
 
     const dateChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => setSelectedDate(e.target.value);
 
     return (
-        <>
+        <section className="dark:text-gray-100">
             <h1>Calendar page</h1>
-            <div className="flex">
+            <div className="flex flex-col">
                 <span>Select date:</span>
                 <input type="date" onChange={dateChangeHandler} />
             </div>
-        </>
+        </section>
     );
 };
 
