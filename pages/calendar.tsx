db9bb35fe23fa2bf1ea27258caf66852478c646d
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-10 11:20:20 +0200

    adds basic grid structure to calendar

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -15,17 +15,19 @@ const Calendar = () => {
     const dateChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => setSelectedDate(e.target.value);
 
     return (
-        <section className="dark:text-gray-100">
+        <section className="dark:text-gray-100 lg:max-w-xl">
             <h1>Calendar page</h1>
-            <div className="flex flex-col">
+            <div className="flex flex-col pb-3">
                 <span>Select date:</span>
                 <input type="date" onChange={dateChangeHandler} />
             </div>
             {week && (
-                <div className="grid grid-cols-7 ">
+                <div className="grid h-96 grid-cols-7 gap-2">
                     {week.map((day, i) => (
-                        <div className="bg-purple-400 " key={day.id}>
-                            {day.id}
+                        <div className="flex h-full flex-col gap-2" key={day.id}>
+                            <div className="rounded-lg bg-purple-400">10am</div>
+                            <div className="rounded-lg bg-purple-400">1pm</div>
+                            <div className="rounded-lg bg-purple-400">4pm</div>
                         </div>
                     ))}
                 </div>
