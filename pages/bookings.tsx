ef9540cf176467bb41cbd4a5575b44105b14de05
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-13 13:44:06 +0200

    adds boolean value to state to comfirm checked password

--- pages/bookings.tsx
+++ pages/bookings.tsx
@@ -1,7 +1,9 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 const Bookings = () => {
+    const [dasswordAuthenticated, setPasswordAuthenticated] = useState(false);
     const router = useRouter();
     const { data: session } = useSession({
         required: true,
@@ -9,6 +11,23 @@ const Bookings = () => {
             router.replace('/signin');
         },
     });
+
+    const checkPassword = async () => {
+        const res = await fetch('/api/check-password', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ password }),
+        });
+        const data = await res.json();
+        if (data.admin) {
+            setPasswordAuthenticated(true);
+        } else {
+            setError('Incorrect password');
+        }
+    };
 
     return <div>Bookings</div>;
 };
