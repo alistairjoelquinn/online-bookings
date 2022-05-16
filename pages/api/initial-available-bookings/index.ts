52e27bc9f530828844fea96ec0eb8e713038cc2a
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-16 15:10:34 +0200

    all calendar data received at once from api on comp mount

---
+++ pages/api/initial-available-bookings/index.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import connectToDatabase from '../../../lib/mongodb';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const { db } = await connectToDatabase();
+
+    const data = await db.collection('booked').find().toArray();
+
+    return res.json(data);
+};
