9819c41b6943d2df90ea86da8922513f2d052717
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-16 16:00:24 +0200

    blue bar can be blocked out to indicate allowed time slots

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,22 +1,28 @@
 import { useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
 
-import { getWeek, getCurrentDate, scheduleTimes, getInitialAvailableTimes } from '../lib/dates';
+import {
+    getWeek,
+    getCurrentDate,
+    scheduleTimesLabel,
+    getInitialAvailableTimes,
+    generateScheduleTimes,
+} from '../lib/dates';
 import { FormattedDate } from '../models/calendar';
 
 const Calendar = () => {
     const [selectedDate, setSelectedDate] = useState(getCurrentDate);
     const week = useMemo<FormattedDate[] | null>(() => getWeek(selectedDate), [selectedDate]);
 
-    const { status, data } = useQuery('initial-available-times', getInitialAvailableTimes);
+    const { status, data: bookingData } = useQuery('initial-available-times', getInitialAvailableTimes);
 
-    console.log('data: ', data);
+    console.log('bookingData: ', bookingData);
 
     const dateChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSelectedDate(e.target.value);
     };
 
-    if (status === 'loading') return <div className="spin" />;
+    if (status === 'loading' || !week) return <div className="spin" />;
     if (status === 'error') return <span>Oops, something unexpected went wrong!</span>;
 
     return (
@@ -34,14 +40,27 @@ const Calendar = () => {
                             <h1 className="py-3 text-center">{day.value}</h1>
                             <div
                                 onClick={() => setSelectedDate(day.id)}
-                                className={`flex h-full w-36 flex-col justify-between rounded-lg border-2 border-purple-300 bg-white py-2 px-1 text-xs shadow-md ${
+                                className={`relative z-10 flex h-full w-36 flex-col justify-between rounded-lg border-2 border-purple-300 bg-white py-2 px-1 text-xs shadow-md ${
                                     day.id === selectedDate
                                         ? 'ring-2 ring-yellow-300 ring-opacity-75 ring-offset-2 dark:ring-offset-gray-900'
                                         : ''
                                 }`}
                             >
-                                {scheduleTimes.map((time: string, i: number) => (
-                                    <span className="pb-3 text-gray-600 dark:text-gray-400" key={time + i}>
+                                <div className="absolute top-0 left-0 my-4 h-full w-full px-1">
+                                    {generateScheduleTimes(day.id).map(time => {
+                                        console.log('time: ', time);
+                                        return (
+                                            <div key={time} className="h-2 bg-blue-300">
+                                                {/* test */}
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                                {scheduleTimesLabel.map((time: string, i: number) => (
+                                    <span
+                                        className="relative z-20 bg-transparent pb-3 text-gray-600 dark:text-gray-400"
+                                        key={time + i}
+                                    >
                                         {time}
                                     </span>
                                 ))}
--- lib/dates.ts
+++ lib/dates.ts
@@ -38,7 +38,55 @@ const getCurrentDate = () =>
         .reverse()
         .join('-');
 
-const scheduleTimes = ['- 07:00am', '-', '-', '- 10:00am', '-', '-', '- 01:00pm', '-', '-', '- 04:00pm', '-', '-'];
+const scheduleTimesLabel = ['- 07:00am', '-', '-', '- 10:00am', '-', '-', '- 01:00pm', '-', '-', '- 04:00pm', '-', '-'];
+
+const generateScheduleTimes = (date: string) => [
+    `${date}T07:00:00.000Z`,
+    `${date}T07:15:00.000Z`,
+    `${date}T07:30:00.000Z`,
+    `${date}T07:45:00.000Z`,
+    `${date}T08:00:00.000Z`,
+    `${date}T08:15:00.000Z`,
+    `${date}T08:30:00.000Z`,
+    `${date}T08:45:00.000Z`,
+    `${date}T09:00:00.000Z`,
+    `${date}T09:15:00.000Z`,
+    `${date}T09:30:00.000Z`,
+    `${date}T09:45:00.000Z`,
+    `${date}T10:00:00.000Z`,
+    `${date}T10:15:00.000Z`,
+    `${date}T10:30:00.000Z`,
+    `${date}T10:45:00.000Z`,
+    `${date}T11:00:00.000Z`,
+    `${date}T11:15:00.000Z`,
+    `${date}T11:30:00.000Z`,
+    `${date}T11:45:00.000Z`,
+    `${date}T12:00:00.000Z`,
+    `${date}T12:15:00.000Z`,
+    `${date}T12:30:00.000Z`,
+    `${date}T12:45:00.000Z`,
+    `${date}T13:00:00.000Z`,
+    `${date}T13:15:00.000Z`,
+    `${date}T13:30:00.000Z`,
+    `${date}T13:45:00.000Z`,
+    `${date}T14:00:00.000Z`,
+    `${date}T14:15:00.000Z`,
+    `${date}T14:30:00.000Z`,
+    `${date}T14:45:00.000Z`,
+    `${date}T15:00:00.000Z`,
+    `${date}T15:15:00.000Z`,
+    `${date}T15:30:00.000Z`,
+    `${date}T15:45:00.000Z`,
+    `${date}T16:00:00.000Z`,
+    `${date}T16:15:00.000Z`,
+    `${date}T16:30:00.000Z`,
+    `${date}T16:45:00.000Z`,
+    `${date}T17:00:00.000Z`,
+    `${date}T17:15:00.000Z`,
+    `${date}T17:30:00.000Z`,
+    `${date}T17:45:00.000Z`,
+    `${date}T18:00:00.000Z`,
+];
 
 const getInitialAvailableTimes = () =>
     Promise.all([
@@ -46,4 +94,4 @@ const getInitialAvailableTimes = () =>
         fetch('/api/initial-available-bookings').then(res => res.json()),
     ]);
 
-export { getWeek, getCurrentDate, scheduleTimes, getInitialAvailableTimes };
+export { getWeek, getCurrentDate, scheduleTimesLabel, getInitialAvailableTimes, generateScheduleTimes };
