c7490b81275cf8b0d7460dd85af7d165303e3d39
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-16 20:54:17 +0200

    moves slot check logic to separate file

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
+import { checkSlot } from '../lib/check-calendar-slot';
 
 import {
     getWeek,
@@ -9,62 +10,6 @@ import {
     generateScheduleTimes,
 } from '../lib/dates';
 import { AvailableTime, FormattedDate } from '../models/calendar';
-
-const checkSlot = (time: string, bookingData: any, dayId: string, index: number) => {
-    const dayTimeArray = generateScheduleTimes(dayId);
-
-    const nextSlot = dayTimeArray[index + 1];
-
-    let currentStatus = 'open-slot';
-    let nextStatus = 'open-slot';
-
-    bookingData?.[0].forEach((item: AvailableTime) => {
-        if (new Date(time) >= new Date(item.start) && new Date(time) <= new Date(item.end)) {
-            currentStatus = 'available';
-        }
-    });
-
-    bookingData?.[1].forEach((item: AvailableTime) => {
-        if (new Date(time) >= new Date(item.start) && new Date(time) < new Date(item.end)) {
-            currentStatus = 'booked';
-        }
-    });
-
-    bookingData?.[0].forEach((item: AvailableTime) => {
-        if (new Date(nextSlot) >= new Date(item.start) && new Date(nextSlot) <= new Date(item.end)) {
-            nextStatus = 'available';
-        }
-    });
-
-    bookingData?.[1].forEach((item: AvailableTime) => {
-        if (new Date(nextSlot) >= new Date(item.start) && new Date(nextSlot) < new Date(item.end)) {
-            nextStatus = 'booked';
-        }
-    });
-
-    let closingBlockFormat;
-
-    if (
-        (currentStatus === 'available' && nextStatus === 'booked') ||
-        (currentStatus === 'available' && nextStatus === 'open-slot')
-    ) {
-        closingBlockFormat = 'available-block-end';
-    }
-    if (currentStatus === 'available' && nextStatus === 'available') {
-        closingBlockFormat = 'available-block-center';
-    }
-    if (
-        (currentStatus === 'booked' && nextStatus === 'available') ||
-        (currentStatus === 'booked' && nextStatus === 'open-slot')
-    ) {
-        closingBlockFormat = 'booked-block-end';
-    }
-    if (currentStatus === 'booked' && nextStatus === 'booked') {
-        closingBlockFormat = 'booked-block-center';
-    }
-
-    return [currentStatus, closingBlockFormat];
-};
 
 const Calendar = () => {
     const [selectedDate, setSelectedDate] = useState(getCurrentDate);
