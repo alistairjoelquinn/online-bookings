4c76a32a1a968e8a6191c119add28bfffc5fc59b
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-16 14:16:36 +0200

    moves all input field styles to single global

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -17,12 +17,7 @@ const Calendar = () => {
                 <span className="text-md py-3 pr-5 font-extrabold dark:text-gray-100 md:text-2xl lg:text-3xl">
                     Select a date:
                 </span>
-                <input
-                    value={selectedDate}
-                    type="date"
-                    onChange={dateChangeHandler}
-                    className="rounded-lg border-2 border-purple-300 bg-white px-5 py-3 text-black shadow-md "
-                />
+                <input value={selectedDate} type="date" onChange={dateChangeHandler} className="input" />
             </div>
             {week && (
                 <div className="grid grid-cols-5 gap-4">
@@ -31,14 +26,14 @@ const Calendar = () => {
                             <h1 className="py-3 text-center">{day.value}</h1>
                             <div
                                 onClick={() => setSelectedDate(day.id)}
-                                className={`calendar-day flex w-36 flex-col justify-between rounded-lg py-2 px-1 text-xs ${
+                                className={`flex h-full w-36 flex-col justify-between rounded-lg border-2 border-purple-300 bg-white py-2 px-1 text-xs shadow-md ${
                                     day.id === selectedDate
                                         ? 'ring-2 ring-yellow-300 ring-opacity-75 ring-offset-2 dark:ring-offset-gray-900'
                                         : ''
                                 }`}
                             >
                                 {scheduleTimes.map((time: string, i: number) => (
-                                    <span className="pb-3" key={time + i}>
+                                    <span className="pb-3 text-gray-600 dark:text-gray-400" key={time + i}>
                                         {time}
                                     </span>
                                 ))}
