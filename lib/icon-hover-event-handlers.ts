5ac008c6342910e278a10b601fc5166b7e13291d
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-18 15:33:38 +0200

    highlights bookings svg icons based on dark mode

--- lib/icon-hover-event-handlers.ts
+++ lib/icon-hover-event-handlers.ts
@@ -1,5 +1,11 @@
 export default () => ({
-    onMouseOver: (e: any) => (e.currentTarget.children[0].style.stroke = 'red'),
+    onMouseOver: (e: any) => {
+        if (document.documentElement.classList.contains('dark')) {
+            e.currentTarget.children[0].style.stroke = 'white';
+        } else {
+            e.currentTarget.children[0].style.stroke = 'black';
+        }
+    },
     onMouseOut: (e: any) => {
         e.currentTarget.children[0].style.stroke = '';
     },
