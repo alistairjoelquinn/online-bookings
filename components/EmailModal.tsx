586a866dca8a19d67314cca73fc9e433782dc06d
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-18 16:20:13 +0200

    refactors email modal

--- components/EmailModal.tsx
+++ components/EmailModal.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const EmailModal = () => (
-    <div className="absolute inset-0 z-30 flex items-center justify-center bg-gray-600 opacity-40">
-        <div className="rounded-lg border-2 border-gray-800 bg-white p-28 opacity-100">
+    <>
+        <div className="absolute inset-0 z-30 flex items-center justify-center bg-gray-600 opacity-40" />
+        <div className="absolute left-1/2 top-1/2 z-40 max-w-screen-lg -translate-x-1/2 -translate-y-1/2 rounded-lg border-2 border-gray-800 bg-white p-20 opacity-100">
             <p className="text-3xl tracking-wide text-gray-900">englishwithfelicity@gmail.com</p>
         </div>
-    </div>
+    </>
 );
 
 export default EmailModal;
