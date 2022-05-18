6cecaeded35cde308593fb64bbcd1052cf9dceb1
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-18 17:06:43 +0200

    completes email modal

--- components/EmailModal.tsx
+++ components/EmailModal.tsx
@@ -19,8 +19,8 @@ const EmailModal = ({ setShowEmailModal, showEmailModal }: Props) => (
             className="absolute inset-0 z-30 bg-gray-600 opacity-70"
             onClick={() => setShowEmailModal(false)}
         />
-        <div className="absolute left-1/2 top-1/2 z-40 max-w-screen-lg -translate-x-1/2 -translate-y-1/2 rounded-lg border-2 border-gray-800 bg-white p-20 opacity-100">
-            <p className="text-3xl tracking-wide text-gray-900">englishwithfelicity@gmail.com</p>
+        <div className="absolute left-1/2 top-1/2 z-40 max-w-screen-lg -translate-x-1/2 -translate-y-1/2 rounded-lg border-2 border-purple-400 bg-white p-20 opacity-100">
+            <p className="text-3xl tracking-wide text-gray-600">englishwithfelicity@gmail.com</p>
         </div>
     </>
 );
