7845f7d2bf624b10ed4af4bbfd1c6458c60d2056
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-19 10:06:26 +0200

    email modal is mobile responsive

--- components/EmailModal.tsx
+++ components/EmailModal.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 const EmailModal = ({ setShowEmailModal }: Props) => (
     <>
-        <div className="absolute inset-0 z-30 bg-gray-600 opacity-70" onClick={() => setShowEmailModal(false)} />
-        <div className="absolute left-1/2 top-1/2 z-40 max-w-screen-lg -translate-x-1/2 -translate-y-1/2 rounded-lg border-2 border-purple-400 bg-white p-20 opacity-100">
-            <p className="text-3xl tracking-wide text-gray-600">englishwithfelicity@gmail.com</p>
+        <div className="fixed inset-0 z-30 bg-gray-600 opacity-70" onClick={() => setShowEmailModal(false)} />
+        <div className="absolute left-1/2 top-1/2 z-40 -translate-x-1/2 -translate-y-1/2 rounded-lg border-2 border-purple-400 bg-white p-8 opacity-100 sm:p-16 md:p-20">
+            <p className="text-xl tracking-wide text-gray-600 sm:text-2xl md:text-3xl">englishwithfelicity@gmail.com</p>
         </div>
     </>
 );
