1cf84f03a5f1632ee752eeca101f06d713ad6228
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-19 10:24:15 +0200

    clicking icons writes text to clipboard and displays user feedback

--- components/EmailModal.tsx
+++ components/EmailModal.tsx
@@ -1,21 +1,39 @@
 import { Copy } from 'grommet-icons';
-import React from 'react';
+import React, { useState } from 'react';
 import writeTextToClipboard from '../lib/write-text-to-clipboard';
 
 interface Props {
     setShowEmailModal: (val: boolean) => void;
 }
 
-const EmailModal = ({ setShowEmailModal }: Props) => (
-    <>
-        <div className="fixed inset-0 z-30 bg-gray-600 opacity-70" onClick={() => setShowEmailModal(false)} />
-        <div className="absolute left-1/2 top-1/2 z-40 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-lg border-2 border-purple-400 bg-white px-8 py-10 opacity-100 sm:px-16 md:px-20">
-            <p className="pb-6 text-xl tracking-wide text-gray-600 sm:text-2xl md:text-3xl">
-                englishwithfelicity@gmail.com
-            </p>
-            <Copy onClick={() => writeTextToClipboard('englishwithfelicity@gmail.com')} cursor="pointer" />
-        </div>
-    </>
-);
+const EmailModal = ({ setShowEmailModal }: Props) => {
+    const [isCopied, setIsCopied] = useState(false);
+
+    const copyClickHandler = async () => {
+        try {
+            await writeTextToClipboard('englishwithfelicity@gmail.com');
+            setIsCopied(true);
+            setTimeout(() => setIsCopied(false), 1500);
+        } catch (err) {
+            console.log('browser not compatible');
+        }
+    };
+
+    return (
+        <>
+            <div className="fixed inset-0 z-30 bg-gray-600 opacity-70" onClick={() => setShowEmailModal(false)} />
+            <div className="absolute left-1/2 top-1/2 z-40 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-lg border-2 border-purple-400 bg-white px-8 py-10 opacity-100 sm:px-16 md:px-20">
+                <p className="pb-6 text-xl tracking-wide text-gray-600 sm:text-2xl md:text-3xl">
+                    englishwithfelicity@gmail.com
+                </p>
+                {!isCopied ? (
+                    <Copy onClick={copyClickHandler} cursor="pointer" />
+                ) : (
+                    <p className="font-sans">Copied to clipboard!</p>
+                )}
+            </div>
+        </>
+    );
+};
 
 export default EmailModal;
--- lib/write-text-to-clipboard.ts
+++ lib/write-text-to-clipboard.ts
@@ -1,10 +1,1 @@
-export default (text: string) => {
-    navigator.clipboard.writeText(text).then(
-        () => {
-            console.log('it worked');
-        },
-        () => {
-            console.log(' it failed');
-        },
-    );
-};
+export default (text: string) => navigator.clipboard.writeText(text);
