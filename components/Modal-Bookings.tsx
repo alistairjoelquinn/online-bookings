5d109fa8db28275318624801f4e05c0dec07e7ef
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-19 13:29:58 +0200

    bookings modal displays correctly using wrapper comp

--- components/Modal-Bookings.tsx
+++ components/Modal-Bookings.tsx
@@ -1,6 +1,11 @@
-const BookingsModal = () => {
-    console.log('Bookings modal');
-    return <div>BookingsModal</div>;
-};
+import ModalContainer from './Modal-Container';
+
+interface Props {
+    closeModal: (val: boolean) => void;
+}
+
+const BookingsModal = ({ closeModal }: Props) => (
+    <ModalContainer closeModal={closeModal}>Bookings Modal</ModalContainer>
+);
 
 export default BookingsModal;
