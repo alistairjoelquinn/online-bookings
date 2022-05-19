3d17014f674f1747e6ebdebb5d233d850290babb
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-19 11:29:39 +0200

    home button on bookings page is mobile responsive

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
+
 import EmailModal from './EmailModal';
 import Footer from './Footer';
 
 const Layout: React.FC = ({ children }) => {
+    const router = useRouter();
+    console.log('router: ', router);
     const [showEmailModal, setShowEmailModal] = useState(false);
 
     return (
         <section className="flex min-h-screen w-screen flex-col bg-white from-gray-100 text-gray-900 dark:bg-gray-900 dark:from-gray-900 md:flex-row md:bg-gradient-to-r">
-            <div className="m-0 h-full w-screen overflow-hidden rounded-none shadow-md md:m-16 md:h-1/5 md:w-1/5 md:rounded-2xl md:shadow-xl">
-                <img
-                    src="img/main-image.jpg"
-                    className="h-full w-full rounded-none object-cover md:rounded-2xl"
-                    alt="Felicity Quinn"
-                />
+            <div className="item-center m-0 flex h-full w-screen flex-col overflow-hidden md:m-16 md:h-1/5 md:w-1/5">
+                <div className="rounded-none shadow-md md:rounded-2xl md:shadow-xl">
+                    <img
+                        src="img/main-image.jpg"
+                        className="h-full w-full rounded-none object-cover md:rounded-2xl"
+                        alt="Felicity Quinn"
+                    />
+                </div>
+                {router.pathname === '/bookings' && (
+                    <Link href="/" passHref>
+                        <button type="button" className="btn-invert mx-4 my-8 md:mx-0 md:w-full">
+                            Home
+                        </button>
+                    </Link>
+                )}
             </div>
             {showEmailModal && <EmailModal setShowEmailModal={setShowEmailModal} />}
             {children}
