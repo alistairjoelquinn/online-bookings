71c4047baaf369efb0575462888bad050cedc8db
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-19 10:12:08 +0200

    adds clipboard icon to the email modal

--- components/EmailModal.tsx
+++ components/EmailModal.tsx
@@ -1,3 +1,4 @@
+import { Copy } from 'grommet-icons';
 import React from 'react';
 
 interface Props {
@@ -7,8 +8,11 @@ interface Props {
 const EmailModal = ({ setShowEmailModal }: Props) => (
     <>
         <div className="fixed inset-0 z-30 bg-gray-600 opacity-70" onClick={() => setShowEmailModal(false)} />
-        <div className="absolute left-1/2 top-1/2 z-40 -translate-x-1/2 -translate-y-1/2 rounded-lg border-2 border-purple-400 bg-white p-8 opacity-100 sm:p-16 md:p-20">
-            <p className="text-xl tracking-wide text-gray-600 sm:text-2xl md:text-3xl">englishwithfelicity@gmail.com</p>
+        <div className="absolute left-1/2 top-1/2 z-40 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-lg border-2 border-purple-400 bg-white px-8 py-10 opacity-100 sm:px-16 md:px-20">
+            <p className="pb-6 text-xl tracking-wide text-gray-600 sm:text-2xl md:text-3xl">
+                englishwithfelicity@gmail.com
+            </p>
+            <Copy cursor="pointer" />
         </div>
     </>
 );
