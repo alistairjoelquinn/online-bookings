e035d430ebd7b9769978ad9df38df6e03dc874c7
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-19 10:35:03 +0200

    improves padding on mobile view on login page

--- components/Login.tsx
+++ components/Login.tsx
@@ -31,30 +31,32 @@ const Login = ({ setError }: Props) => {
 
     return (
         <div className="relative z-10 pt-6 md:h-screen md:pt-16">
-            {status === 'unauthenticated' && router.query.login && (
-                <p className="para mb-4 text-red-400">Sorry, that was the wrong password. Please try again.</p>
-            )}
-            <p className="para my-0">
-                To use the online booking portal you need a password. You can request a password by emailing:
-            </p>
-            <p className="block py-5 text-center font-sans text-xl tracking-wider text-gray-600 dark:text-gray-400">
-                englishwithfelicity@gmail.com
-            </p>
-            <p className="para mt-0 mb-4">
-                This password will log you in for 2 months, so you won&apos;t need to enter it again for subsequent
-                bookings!
-            </p>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                <input
-                    name="password"
-                    type="password"
-                    className="input w-full"
-                    onChange={e => setPassword(e.target.value)}
-                />
-                <button type="submit" className="btn">
-                    Submit
-                </button>
-            </form>
+            <section className="max-w-6xl overflow-scroll px-4 pt-0 pb-6 text-left md:h-4/5 lg:max-w-xl">
+                {status === 'unauthenticated' && router.query.login && (
+                    <p className="para mb-4 text-red-400">Sorry, that was the wrong password. Please try again.</p>
+                )}
+                <p className="para my-0">
+                    To use the online booking portal you need a password. You can request a password by emailing:
+                </p>
+                <p className="block py-5 text-center font-sans text-xl tracking-wider text-gray-600 dark:text-gray-400">
+                    englishwithfelicity@gmail.com
+                </p>
+                <p className="para mt-0 mb-4">
+                    This password will log you in for 2 months, so you won&apos;t need to enter it again for subsequent
+                    bookings!
+                </p>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                    <input
+                        name="password"
+                        type="password"
+                        className="input w-full"
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <button type="submit" className="btn">
+                        Submit
+                    </button>
+                </form>
+            </section>
         </div>
     );
 };
