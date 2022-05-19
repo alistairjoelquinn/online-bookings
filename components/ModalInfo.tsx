e478e5a66f4adfabdba6c68a92a8482c43dcccff
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-19 16:30:08 +0200

    improves info modal on mobile

--- components/ModalInfo.tsx
+++ components/ModalInfo.tsx
@@ -11,13 +11,13 @@ const InfoModal = ({ closeModal }: Props) => (
                 Select a date to see my availability. When you select a date the full week will be displayed. The
                 currently selected date is always outlined in yellow.
             </p>
-            <div className="flex justify-start md:pb-4 md:pl-8">
+            <div className="flex flex-col-reverse items-center justify-start pb-2 sm:flex-row md:pb-4 md:pl-8">
                 <div className="mx-4 h-8 w-8 rounded-full bg-green-300" />
-                <p className="modal">Available times are highlighted in green</p>
+                <p className="modal mb-2 sm:mb-auto">Available times are highlighted in green</p>
             </div>
-            <div className="flex items-center justify-start pb-8 md:pl-8">
+            <div className="flex flex-col-reverse items-center justify-start pb-2 sm:flex-row md:pl-8">
                 <div className="mx-4 h-8 w-8 rounded-full bg-purple-200" />
-                <p className="modal">Booked slots are blocked out in purple</p>
+                <p className="modal mb-2 sm:mb-auto">Booked slots are blocked out in purple</p>
             </div>
             <p className="modal pb-8">
                 Once you have found a suitable time either click on the day you have selected, or click on the plus
