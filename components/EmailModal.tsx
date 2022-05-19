ac3796ca8e2d9b10d034450dcd9eec71026c6e72
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-19 10:36:17 +0200

    email modal is always fixed to center of screen on mobile

--- components/EmailModal.tsx
+++ components/EmailModal.tsx
@@ -22,7 +22,7 @@ const EmailModal = ({ setShowEmailModal }: Props) => {
     return (
         <>
             <div className="fixed inset-0 z-30 bg-gray-600 opacity-70" onClick={() => setShowEmailModal(false)} />
-            <div className="absolute left-1/2 top-1/2 z-40 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-lg border-2 border-purple-400 bg-white px-8 py-10 opacity-100 sm:px-16 md:px-20">
+            <div className="fixed left-1/2 top-1/2 z-40 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-lg border-2 border-purple-400 bg-white px-8 py-10 opacity-100 sm:px-16 md:px-20">
                 <p className="pb-6 text-xl tracking-wide text-gray-600 sm:text-2xl md:text-3xl">
                     englishwithfelicity@gmail.com
                 </p>
