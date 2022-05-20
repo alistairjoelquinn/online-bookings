316da23d224ebfece9546a60652abd594a43716b
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-20 16:44:06 +0200

    refac admin panel

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -6,18 +6,19 @@ import { getAvailableTimesAndBookings } from '../lib/dates';
 import type { AvailableTime, BookedTime } from '../models/calendar';
 
 const Admin = () => {
-    const { status, data } = useQuery('get-available-times-and-bookings', getAvailableTimesAndBookings);
+    const { status, data }: { status: string; data: [AvailableTime[], BookedTime[]] | undefined } = useQuery(
+        'get-available-times-and-bookings',
+        getAvailableTimesAndBookings,
+    );
 
-    const [available, booked] = data || [undefined, undefined];
-
-    console.log('available , booked: ', available, booked);
+    const [available, booked] = data || [null, null];
 
     return (
         <div className="relative z-10 animate-reveal pt-6 dark:text-gray-100 md:h-screen md:pt-16">
             <section className="max-w-6xl overflow-scroll px-4 pt-0 pb-6 text-left md:h-4/5 lg:max-w-xl">
                 <h3 className="mb-6 text-5xl font-extrabold sm:text-5xl md:text-4xl lg:text-5xl">Admin Page</h3>
                 <h4 className="my-4 border-b-2 border-gray-500 text-lg">Upcoming bookings</h4>
-                {booked?.filter((item: BookedTime) => new Date(item.start) > new Date()).length < 1 && (
+                {booked && booked.filter((item: BookedTime) => new Date(item.start) > new Date()).length < 1 && (
                     <p>No upcoming bookings...</p>
                 )}
                 {booked
@@ -29,7 +30,7 @@ const Admin = () => {
                     <AvailableCard item={item} key={item._id} />
                 ))}
                 <h4 className="my-4 border-b-2 border-gray-500 text-lg">Previous bookings</h4>
-                {booked?.filter((item: BookedTime) => new Date(item.start) < new Date()).length < 1 && (
+                {booked && booked.filter((item: BookedTime) => new Date(item.start) < new Date()).length < 1 && (
                     <p>No upcoming bookings...</p>
                 )}
                 {booked
