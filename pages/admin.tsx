5c2ebb1f6fa50542fc3b6377b6255809307b9014
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-20 16:08:10 +0200

    admin panel text changes color correctly in dark mode

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -11,16 +11,14 @@ const Admin = () => {
     console.log('available , booked: ', available, booked);
 
     return (
-        <div className="relative z-10 animate-reveal pt-6 md:h-screen md:pt-16">
+        <div className="relative z-10 animate-reveal pt-6 dark:text-gray-100 md:h-screen md:pt-16">
             <section className="max-w-6xl overflow-scroll px-4 pt-0 pb-6 text-left md:h-4/5 lg:max-w-xl">
-                <h3 className="mb-6 text-5xl font-extrabold dark:text-gray-100 sm:text-5xl md:text-4xl lg:text-5xl">
-                    Admin Page
-                </h3>
+                <h3 className="mb-6 text-5xl font-extrabold sm:text-5xl md:text-4xl lg:text-5xl">Admin Page</h3>
                 <h4 className="my-4 border-b-2 border-gray-500 text-lg">Upcoming bookings</h4>
                 {booked?.map((item: BookedTime) => (
                     <div
                         key={item._id}
-                        className="my-2 flex w-80 animate-reveal flex-col items-center rounded-lg border-2 border-purple-400 bg-white px-2 py-4 opacity-100 dark:bg-gray-900 sm:w-auto"
+                        className="my-2 flex w-80 animate-reveal flex-col items-start gap-2 rounded-lg border-2 border-purple-400 bg-white px-2 py-4 opacity-100 dark:bg-gray-900 sm:w-auto"
                     >
                         <p>{item.name}</p>
                         <p>{item.email}</p>
