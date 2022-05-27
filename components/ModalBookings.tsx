c10adcc6e7f64c49aa4924e482cf4a4ab5a44ab4
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-27 10:57:07 +0200

    all input fields are required

--- components/ModalBookings.tsx
+++ components/ModalBookings.tsx
@@ -51,35 +51,42 @@ const ModalBookings = ({ closeModal, date }: Props) => {
                     <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
                         Select a date:
                     </p>
-                    <input value={date} name="date" type="date" onChange={updateBookingData} className="input w-96" />
+                    <input
+                        value={date}
+                        required
+                        name="date"
+                        type="date"
+                        onChange={updateBookingData}
+                        className="input w-96"
+                    />
                 </div>
                 <div className="flex w-full justify-end">
                     <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
                         From:
                     </p>
-                    <input name="from" type="time" onChange={updateBookingData} className="input mr-7 w-36" />
+                    <input name="from" required type="time" onChange={updateBookingData} className="input mr-7 w-36" />
                     <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
                         Until:
                     </p>
-                    <input name="until" type="time" onChange={updateBookingData} className="input w-36" />
+                    <input name="until" required type="time" onChange={updateBookingData} className="input w-36" />
                 </div>
                 <div className="flex w-full justify-end">
                     <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
                         Full Name:
                     </p>
-                    <input name="fullname" type="text" onChange={updateBookingData} className="input w-96" />
+                    <input name="fullname" required type="text" onChange={updateBookingData} className="input w-96" />
                 </div>
                 <div className="flex w-full justify-end">
                     <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
                         Email Address:
                     </p>
-                    <input name="email" type="email" onChange={updateBookingData} className="input w-96" />
+                    <input name="email" required type="email" onChange={updateBookingData} className="input w-96" />
                 </div>
                 <div className="flex w-full justify-end">
                     <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
                         Booking Type:
                     </p>
-                    <select name="type" onChange={updateBookingData} className="input w-96 pr-4">
+                    <select required name="type" onChange={updateBookingData} className="input w-96 pr-4">
                         <option value="">Choose an option...</option>
                         <option value="cefr">CEFR level class: A2 - C1</option>
                         <option value="conversation">Conversation class</option>
