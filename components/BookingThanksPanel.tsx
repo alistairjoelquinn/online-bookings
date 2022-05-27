5855726f95615e33afbfbcd8b667c911e4618ec2
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-27 12:02:32 +0200

    completes thanks panel

--- components/BookingThanksPanel.tsx
+++ components/BookingThanksPanel.tsx
@@ -5,15 +5,26 @@ interface Props {
     data: BookingData;
 }
 
+const lessonTypes = {
+    cefr: 'CEFR level class',
+    business: 'Busniess English Class',
+    conversation: 'Conversation Class',
+    '': '',
+};
+
 const BookingThanksPanel = ({ data }: Props) => (
-    <>
-        <p>Thank you {data.name}</p>
-        <p>You have booked {data.type}</p>
-        <p>
-            For the {data.date} from {data.from} till {data.until}
+    <div className="flex flex-col items-center gap-2">
+        <p className="modal font-semibold">Thank you {data.name}</p>
+        <p className="modal">
+            You have booked a {lessonTypes[data.type]} from {data.from} till {data.until} on{' '}
+            {new Date(data.date).toLocaleDateString('en-uk', { weekday: 'short', day: 'numeric', month: '2-digit' })}
         </p>
-        <p>I look for to learning with you!</p>
-    </>
+        <p className="modal">
+            If you have any questions just email me at{' '}
+            <span className="text-lg font-normal tracking-wide">englishwithfelicity@gmail.com</span>
+        </p>
+        <p className="modal">I look forward to learning with you!</p>
+    </div>
 );
 
 export default BookingThanksPanel;
