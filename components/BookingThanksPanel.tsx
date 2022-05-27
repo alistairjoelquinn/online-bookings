661da21b7b0b40d1f78b32c8952c558e63049416
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-27 11:45:44 +0200

    adds dynmaic values to booking thanks panel

--- components/BookingThanksPanel.tsx
+++ components/BookingThanksPanel.tsx
@@ -5,9 +5,15 @@ interface Props {
     data: BookingData;
 }
 
-const BookingThanksPanel = ({ data }: Props) => {
-    console.log('data: ', data);
-    return <p>Thank you for submitting your booking</p>;
-};
+const BookingThanksPanel = ({ data }: Props) => (
+    <>
+        <p>Thank you {data.name}</p>
+        <p>You have booked {data.type}</p>
+        <p>
+            For the {data.date} from {data.from} till {data.until}
+        </p>
+        <p>I look for to learning with you!</p>
+    </>
+);
 
 export default BookingThanksPanel;
