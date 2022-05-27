5614722c9df547f684ebe123696ee72b5bb80fa3
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-27 11:25:31 +0200

    adds api route for handling user form submission

---
+++ pages/api/submit-user-booking/index.ts
@@ -0,0 +1,22 @@
+import { getSession } from 'next-auth/react';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { validateIncomingValues } from '../../../lib/validate-form-values';
+import connectToDatabase from '../../../lib/mongodb';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getSession({ req });
+
+    if (!session) return res.status(401);
+
+    const error = validateIncomingValues(req.body);
+
+    if (error) return res.json({ error });
+
+    const { db } = await connectToDatabase();
+
+    const { acknowledged } = await db.collection('rsvps').insertOne(req.body);
+
+    console.log('acknowledged: ', acknowledged);
+    return res.status(200).json({ success: true });
+};
