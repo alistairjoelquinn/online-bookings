c6fd0f06919c83e961f3e45829c25bcb28e5cb42
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-05-31 09:26:27 +0200

    invalidates cache after admin updates hours

--- components/ModalAdmin.tsx
+++ components/ModalAdmin.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useQueryClient } from 'react-query';
 
 import ModalContainer from './ModalContainer';
 import type { AvailableTime } from '../models/calendar';
@@ -8,6 +9,7 @@ interface Props {
 }
 
 const ModalAdmin = ({ closeModal }: Props) => {
+    const queryClient = useQueryClient();
     const [error, setError] = useState('');
     const [submitted, setSubmitted] = useState(false);
     const [availableTime, setAvailableTime] = useState<AvailableTime>({
@@ -28,7 +30,7 @@ const ModalAdmin = ({ closeModal }: Props) => {
         if (Object.values(availableTime).filter(Boolean).length !== Object.values(availableTime).length) {
             setError('Remember to fill out all the input fields...');
         }
-        const res = await fetch('/api/submit-user-booking', {
+        const res = await fetch('/api/submit-admin-hours', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
@@ -41,6 +43,7 @@ const ModalAdmin = ({ closeModal }: Props) => {
             setError(data.error);
         } else if (data.success === true) {
             setSubmitted(true);
+            queryClient.invalidateQueries('get-available-times-and-bookings');
         } else {
             setError('Something unexpected went wrong!');
         }
@@ -49,34 +52,54 @@ const ModalAdmin = ({ closeModal }: Props) => {
     return (
         <ModalContainer closeModal={closeModal}>
             <div className="flex flex-col justify-evenly text-sm md:text-base">
-                <p className="modal mb-4 text-xl font-semibold">{!error ? 'Enter your availability:' : error}</p>
-                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                    <div className="flex w-full justify-end">
-                        <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
-                            Select a date:
+                {!submitted ? (
+                    <>
+                        <p className="modal mb-4 text-xl font-semibold">
+                            {!error ? 'Enter your availability:' : error}
                         </p>
-                        <input required name="date" type="date" onChange={updateAvailableTime} className="input w-96" />
-                    </div>
-                    <div className="flex w-full justify-end">
-                        <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
-                            From:
-                        </p>
-                        <input
-                            name="start"
-                            required
-                            type="time"
-                            onChange={updateAvailableTime}
-                            className="input mr-7 w-36"
-                        />
-                        <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
-                            Until:
-                        </p>
-                        <input name="end" required type="time" onChange={updateAvailableTime} className="input w-36" />
-                    </div>
-                    <button type="submit" className="btn">
-                        Submit
-                    </button>
-                </form>
+                        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                            <div className="flex w-full justify-end">
+                                <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
+                                    Select a date:
+                                </p>
+                                <input
+                                    required
+                                    name="date"
+                                    type="date"
+                                    onChange={updateAvailableTime}
+                                    className="input w-96"
+                                />
+                            </div>
+                            <div className="flex w-full justify-end">
+                                <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
+                                    From:
+                                </p>
+                                <input
+                                    name="start"
+                                    required
+                                    type="time"
+                                    onChange={updateAvailableTime}
+                                    className="input mr-7 w-36"
+                                />
+                                <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
+                                    Until:
+                                </p>
+                                <input
+                                    name="end"
+                                    required
+                                    type="time"
+                                    onChange={updateAvailableTime}
+                                    className="input w-36"
+                                />
+                            </div>
+                            <button type="submit" className="btn">
+                                Submit
+                            </button>
+                        </form>
+                    </>
+                ) : (
+                    <p>Good job</p>
+                )}
             </div>
         </ModalContainer>
     );
