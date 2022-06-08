2ca3319565b372be54ab3294add1b28df4e0a35c
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-06-08 10:38:17 +0200

    refactors util for checking booking valid

--- lib/check-booking-valid.ts
+++ lib/check-booking-valid.ts
@@ -4,16 +4,5 @@ export const checkBookingValid = (data: BookedTime, available: AvailableTime[]) 
     const start = new Date(`${data.date}T${data.start}`);
     const end = new Date(`${data.date}T${data.end}`);
 
-    console.log('start, end: ', start, end);
-
-    const result = available.some(x => {
-        console.log('x', x);
-        console.log('start >= new Date(x.start): ', start >= new Date(x.start));
-        console.log('end >= new Date(x.end): ', end >= new Date(x.end));
-        return start >= new Date(x.start) && end <= new Date(x.end);
-    });
-
-    console.log('result: ', result);
-
-    return result;
+    return available.some(x => start >= new Date(x.start) && end <= new Date(x.end));
 };
