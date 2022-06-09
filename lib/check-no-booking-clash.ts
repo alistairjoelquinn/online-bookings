a1fe4d62bb1b5501e14954c9393631035c22693a
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-06-09 11:16:52 +0200

    refactors check no booking clash util

--- lib/check-no-booking-clash.ts
+++ lib/check-no-booking-clash.ts
@@ -1,8 +1,12 @@
-import { AvailableTime, BookedTime } from '../models/calendar';
+import { BookedTime } from '../models/calendar';
 
-export default (data: BookedTime, available: AvailableTime[]) => {
+export default (data: BookedTime, booked: BookedTime[]) => {
     const start = new Date(`${data.date}T${data.start}`);
     const end = new Date(`${data.date}T${data.end}`);
 
-    return available.some(x => start >= new Date(x.start) && end <= new Date(x.end));
+    return booked.some(
+        x =>
+            (start >= new Date(x.start) && start <= new Date(x.end)) ||
+            (end >= new Date(x.start) && end <= new Date(x.end)),
+    );
 };
