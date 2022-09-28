fc2d4f71e550bdd038736c5ba1889b9f6bfed379
alistairjoelquinn <alistairjoelquinn@gmail.com>
2022-09-29 00:18:38 +0200

    refactors layout and modal admin components

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,62 +3,63 @@ import { getCsrfToken, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
 interface Props {
-    setError: (val: string) => void;
+  setError: (val: string) => void;
 }
 
-const Login = ({ setError }: Props) => {
-    const { status } = useSession();
-    const [password, setPassword] = useState('');
-    const router = useRouter();
+export default function Login({ setError }: Props) {
+  const { status } = useSession();
+  const [password, setPassword] = useState('');
+  const router = useRouter();
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const csrfToken = await getCsrfToken();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const csrfToken = await getCsrfToken();
 
-        await fetch('/api/auth/callback/credentials', {
-            method: 'POST',
-            headers: {
-                accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ password, csrfToken }),
-        }).catch(err => {
-            setError(err.message);
-        });
-        window.location.replace('/bookings?login=true');
-    };
+    await fetch('/api/auth/callback/credentials', {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ password, csrfToken }),
+    }).catch(err => {
+      setError(err.message);
+    });
+    window.location.replace('/bookings?login=true');
+  };
 
-    return (
-        <div className="relative z-10 animate-reveal pt-6 md:h-screen md:pt-16">
-            <section className="max-w-6xl px-4 pt-0 pb-6 text-left md:h-4/5 lg:max-w-xl">
-                {status === 'unauthenticated' && router.query.login && (
-                    <p className="para mb-4 mt-0 text-red-400">Sorry, that was the wrong password. Please try again.</p>
-                )}
-                <p className="para my-0">
-                    To use the online booking portal you need a password. You can request a password by emailing:
-                </p>
-                <p className="block py-5 text-center font-sans text-xl tracking-wider text-gray-600 dark:text-gray-400">
-                    englishwithfelicity@gmail.com
-                </p>
-                <p className="para mt-0 mb-4">
-                    This password will log you in for 2 months, so you won&apos;t need to enter it again for subsequent
-                    bookings!
-                </p>
-                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                    <input
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        className="input w-full"
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                    <button type="submit" className="btn">
-                        Submit
-                    </button>
-                </form>
-            </section>
-        </div>
-    );
-};
-
-export default Login;
+  return (
+    <div className="relative z-10 animate-reveal pt-6 md:h-screen md:pt-16">
+      <section className="max-w-6xl px-4 pt-0 pb-6 text-left md:h-4/5 lg:max-w-xl">
+        {status === 'unauthenticated' && router.query.login && (
+          <p className="para mb-4 mt-0 text-red-400">
+            Sorry, that was the wrong password. Please try again.
+          </p>
+        )}
+        <p className="para my-0">
+          To use the online booking portal you need a password. You can request
+          a password by emailing:
+        </p>
+        <p className="block py-5 text-center font-sans text-xl tracking-wider text-gray-600 dark:text-gray-400">
+          englishwithfelicity@gmail.com
+        </p>
+        <p className="para mt-0 mb-4">
+          This password will log you in for 2 months, so you won&apos;t need to
+          enter it again for subsequent bookings!
+        </p>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="input w-full"
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+}
--- components/ModalAdmin.tsx
+++ components/ModalAdmin.tsx
@@ -1,119 +1,129 @@
+import ModalContainer from 'components/ModalContainer';
+import isoify from 'lib/isoify';
+import type { AvailableTime } from 'models/calendar';
 import { useState } from 'react';
 import { useQueryClient } from 'react-query';
 
-import ModalContainer from './ModalContainer';
-import type { AvailableTime } from '../models/calendar';
-import isoify from '../lib/isoify';
-
 interface Props {
-    closeModal: (val: boolean) => void;
-    populate: AvailableTime | null;
-    clearState?: () => void;
+  closeModal: (val: boolean) => void;
+  populate: AvailableTime | null;
+  clearState?: () => void;
 }
 
-const ModalAdmin = ({ closeModal, populate, clearState }: Props) => {
-    const queryClient = useQueryClient();
-    const [error, setError] = useState('');
-    const [submitted, setSubmitted] = useState(false);
-    const [availableTime, setAvailableTime] = useState<AvailableTime>(
-        populate
-            ? { ...populate }
-            : {
-                  date: '',
-                  start: '',
-                  end: '',
-              },
-    );
+export default function ModalAdmin({
+  closeModal,
+  populate,
+  clearState,
+}: Props) {
+  const queryClient = useQueryClient();
+  const [error, setError] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const [availableTime, setAvailableTime] = useState<AvailableTime>(
+    populate
+      ? { ...populate }
+      : {
+          date: '',
+          start: '',
+          end: '',
+        },
+  );
 
-    const updateAvailableTime = (e: any) => {
-        setAvailableTime(prevData => ({
-            ...prevData,
-            [e.target.name]: e.target.value,
-        }));
-    };
+  const updateAvailableTime = (e: any) => {
+    setAvailableTime(prevData => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (Object.values(availableTime).filter(Boolean).length !== Object.values(availableTime).length) {
-            setError('Remember to fill out all the input fields...');
-        }
-        const isoDataAdmin = isoify(availableTime);
-        const res = await fetch('/api/submit-admin-hours', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(isoDataAdmin),
-        });
-        const data = await res.json();
-        if (data.error) {
-            setError(data.error);
-        } else if (data.success === true) {
-            setSubmitted(true);
-            queryClient.invalidateQueries('get-available-times-and-bookings');
-        } else {
-            setError('Something unexpected went wrong!');
-        }
-    };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (
+      Object.values(availableTime).filter(Boolean).length !==
+      Object.values(availableTime).length
+    ) {
+      setError('Remember to fill out all the input fields...');
+    }
+    const isoDataAdmin = isoify(availableTime);
+    const res = await fetch('/api/submit-admin-hours', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(isoDataAdmin),
+    });
+    const data = await res.json();
+    if (data.error) {
+      setError(data.error);
+    } else if (data.success === true) {
+      setSubmitted(true);
+      queryClient.invalidateQueries('get-available-times-and-bookings');
+    } else {
+      setError('Something unexpected went wrong!');
+    }
+  };
 
-    return (
-        <ModalContainer clearState={clearState} closeModal={closeModal}>
-            <div className="flex flex-col justify-evenly text-sm md:text-base">
-                {!submitted ? (
-                    <>
-                        <p className="modal mb-4 text-xl font-semibold">
-                            {!error ? `${populate ? 'Edit' : 'Enter'} your availability:` : error}
-                        </p>
-                        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                            <div className="flex w-full justify-end">
-                                <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
-                                    Select a date:
-                                </p>
-                                <input
-                                    defaultValue={availableTime.date}
-                                    required
-                                    name="date"
-                                    type="date"
-                                    onChange={updateAvailableTime}
-                                    className="input w-96"
-                                />
-                            </div>
-                            <div className="flex w-full justify-end">
-                                <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
-                                    From:
-                                </p>
-                                <input
-                                    defaultValue={new Date(availableTime.start).toLocaleTimeString()}
-                                    name="start"
-                                    required
-                                    type="time"
-                                    onChange={updateAvailableTime}
-                                    className="input mr-7 w-36"
-                                />
-                                <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
-                                    Until:
-                                </p>
-                                <input
-                                    defaultValue={new Date(availableTime.end).toLocaleTimeString()}
-                                    name="end"
-                                    required
-                                    type="time"
-                                    onChange={updateAvailableTime}
-                                    className="input w-36"
-                                />
-                            </div>
-                            <button type="submit" className="btn">
-                                {populate ? 'Update' : 'Submit'}
-                            </button>
-                        </form>
-                    </>
-                ) : (
-                    <p>Yayy, that worked...</p>
-                )}
-            </div>
-        </ModalContainer>
-    );
-};
-
-export default ModalAdmin;
+  return (
+    <ModalContainer clearState={clearState} closeModal={closeModal}>
+      <div className="flex flex-col justify-evenly text-sm md:text-base">
+        {!submitted ? (
+          <>
+            <p className="modal mb-4 text-xl font-semibold">
+              {!error
+                ? `${populate ? 'Edit' : 'Enter'} your availability:`
+                : error}
+            </p>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+              <div className="flex w-full justify-end">
+                <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
+                  Select a date:
+                </p>
+                <input
+                  defaultValue={availableTime.date}
+                  required
+                  name="date"
+                  type="date"
+                  onChange={updateAvailableTime}
+                  className="input w-96"
+                />
+              </div>
+              <div className="flex w-full justify-end">
+                <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
+                  From:
+                </p>
+                <input
+                  defaultValue={new Date(
+                    availableTime.start,
+                  ).toLocaleTimeString()}
+                  name="start"
+                  required
+                  type="time"
+                  onChange={updateAvailableTime}
+                  className="input mr-7 w-36"
+                />
+                <p className="modal text-md py-3 pr-5 font-medium dark:text-gray-100 md:text-lg lg:text-xl">
+                  Until:
+                </p>
+                <input
+                  defaultValue={new Date(
+                    availableTime.end,
+                  ).toLocaleTimeString()}
+                  name="end"
+                  required
+                  type="time"
+                  onChange={updateAvailableTime}
+                  className="input w-36"
+                />
+              </div>
+              <button type="submit" className="btn">
+                {populate ? 'Update' : 'Submit'}
+              </button>
+            </form>
+          </>
+        ) : (
+          <p>Yayy, that worked...</p>
+        )}
+      </div>
+    </ModalContainer>
+  );
+}
