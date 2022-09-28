bc2aad5ee04e12277dce5dc4bccf68e79c8e9c90
alistairjoelquinn <alistairjoelquinn@gmail.com>
2022-09-28 23:56:16 +0200

    refactors booking card to use new structure

--- components/BookingCard.tsx
+++ components/BookingCard.tsx
@@ -1,42 +1,40 @@
 import { Edit } from 'grommet-icons';
 import { BookedTime } from '../models/calendar';
 
-const BookingCard = ({
-    item,
-    selectCard,
-    showModal,
-}: {
-    item: BookedTime;
-    selectCard: (val: BookedTime) => void;
-    showModal: (val: boolean) => void;
-}) => (
-    <div className="my-2 flex w-80 animate-reveal items-center justify-between gap-2 rounded-lg border-2 border-purple-400 bg-white px-2 py-4 opacity-100 dark:bg-gray-900 sm:w-auto">
-        <div>
-            <p>{item.name}</p>
-            <p>{item.email}</p>
-            <p>
-                {new Date(item.date).toLocaleDateString('en-uk', {
-                    weekday: 'short',
-                    day: 'numeric',
-                    month: '2-digit',
-                    year: 'numeric',
-                })}
-            </p>
-            <p>
-                {new Date(item.start).toLocaleTimeString().slice(0, -3)} -{' '}
-                {new Date(item.end).toLocaleTimeString().slice(0, -3)}
-            </p>
-        </div>
-        <div className="self-start">
-            <Edit
-                onClick={() => {
-                    selectCard(item);
-                    showModal(true);
-                }}
-                cursor="pointer"
-            />
-        </div>
-    </div>
-);
+interface Props {
+  item: BookedTime;
+  selectCard: (val: BookedTime) => void;
+  showModal: (val: boolean) => void;
+}
 
-export default BookingCard;
+export default function BookingCard({ item, selectCard, showModal }: Props) {
+  return (
+    <div className="my-2 flex w-80 animate-reveal items-center justify-between gap-2 rounded-lg border-2 border-purple-400 bg-white px-2 py-4 opacity-100 dark:bg-gray-900 sm:w-auto">
+      <div>
+        <p>{item.name}</p>
+        <p>{item.email}</p>
+        <p>
+          {new Date(item.date).toLocaleDateString('en-uk', {
+            weekday: 'short',
+            day: 'numeric',
+            month: '2-digit',
+            year: 'numeric',
+          })}
+        </p>
+        <p>
+          {new Date(item.start).toLocaleTimeString().slice(0, -3)} -{' '}
+          {new Date(item.end).toLocaleTimeString().slice(0, -3)}
+        </p>
+      </div>
+      <div className="self-start">
+        <Edit
+          onClick={() => {
+            selectCard(item);
+            showModal(true);
+          }}
+          cursor="pointer"
+        />
+      </div>
+    </div>
+  );
+}
