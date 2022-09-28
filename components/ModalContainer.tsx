dc7c96db20e86453be95a4e6e2a07fd085af579d
alistairjoelquinn <alistairjoelquinn@gmail.com>
2022-09-29 00:21:48 +0200

    refactors modal container to use new structure

--- components/ModalContainer.tsx
+++ components/ModalContainer.tsx
@@ -1,22 +1,26 @@
 interface Props {
-    closeModal: (val: boolean) => void;
-    children?: React.ReactNode;
-    clearState?: () => void;
+  closeModal: (val: boolean) => void;
+  children?: React.ReactNode;
+  clearState?: () => void;
 }
 
-const ModalContainer = ({ children, closeModal, clearState }: Props) => (
+export default function ModalContainer({
+  children,
+  closeModal,
+  clearState,
+}: Props) {
+  return (
     <>
-        <div
-            className="fixed inset-0 z-30 animate-shroud bg-shroud opacity-70"
-            onClick={() => {
-                clearState?.();
-                closeModal(false);
-            }}
-        />
-        <div className="fixed left-1/2 top-1/2 z-40 flex w-80 -translate-x-1/2 -translate-y-1/2 animate-reveal flex-col items-center rounded-lg border-2 border-purple-400 bg-white px-8 py-10 opacity-100 dark:bg-gray-900 sm:w-auto">
-            {children}
-        </div>
+      <div
+        className="fixed inset-0 z-30 animate-shroud bg-shroud opacity-70"
+        onClick={() => {
+          clearState?.();
+          closeModal(false);
+        }}
+      />
+      <div className="fixed left-1/2 top-1/2 z-40 flex w-80 -translate-x-1/2 -translate-y-1/2 animate-reveal flex-col items-center rounded-lg border-2 border-purple-400 bg-white px-8 py-10 opacity-100 dark:bg-gray-900 sm:w-auto">
+        {children}
+      </div>
     </>
-);
-
-export default ModalContainer;
+  );
+}
