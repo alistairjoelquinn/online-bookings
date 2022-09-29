0a086f4770ee85d999601da41a12a756863bb920
alistairjoelquinn <alistairjoelquinn@gmail.com>
2022-09-29 21:14:16 +0200

    refactors validation functions to use new structure

--- lib/validate-form-values.ts
+++ lib/validate-form-values.ts
@@ -1,23 +1,23 @@
 import { BookedTime } from '../models/calendar';
 import { emailRegex, nameRegex } from './validation-regex';
 
-export default (booking: BookedTime) => {
-    let error;
+export default function handler(booking: BookedTime) {
+  let error;
 
-    const n = booking.name.match(nameRegex);
-    const e = booking.email.match(emailRegex);
+  const n = booking.name.match(nameRegex);
+  const e = booking.email.match(emailRegex);
 
-    if (!n) {
-        error = `Oops, there's a problem with the name`;
-    } else if (n && n[0] !== booking.name) {
-        error = `Oops, there's a problem with the name`;
-    } else if (!e) {
-        error = `Oops, there's a problem with the email`;
-    } else if (e && e[0] !== booking.email) {
-        error = `Oops, there's a problem with the email`;
-    } else if (!booking.start || !booking.end || !booking.type || !booking.date) {
-        error = 'Remember to fill out all the fields...';
-    }
+  if (!n) {
+    error = `Oops, there's a problem with the name`;
+  } else if (n && n[0] !== booking.name) {
+    error = `Oops, there's a problem with the name`;
+  } else if (!e) {
+    error = `Oops, there's a problem with the email`;
+  } else if (e && e[0] !== booking.email) {
+    error = `Oops, there's a problem with the email`;
+  } else if (!booking.start || !booking.end || !booking.type || !booking.date) {
+    error = 'Remember to fill out all the fields...';
+  }
 
-    return error;
-};
+  return error;
+}
